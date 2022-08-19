51e778042e4fd1f87957df7bd7e9d144012321f8
ts-oh <tsohlove@gmail.com>
2022-08-19 20:29:03 +0700

    format with prettier

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import { useState } from 'react';
-import Header from './Header';
-import Button from './Button';
-import Statistics from './Statistics';
+import React from 'react'
+import { useState } from 'react'
+import Header from './Header'
+import Button from './Button'
+import Statistics from './Statistics'
 
 function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
 
-  const handleGood = () => {
-    setGood((prevState) => {
-      return prevState + 1;
-    });
-  };
+	const handleGood = () => {
+		setGood((prevState) => {
+			return prevState + 1
+		})
+	}
 
-  const handleNeutral = () => {
-    setNeutral((prevState) => {
-      return prevState + 1;
-    });
-  };
+	const handleNeutral = () => {
+		setNeutral((prevState) => {
+			return prevState + 1
+		})
+	}
 
-  const handleBad = () => {
-    setBad((prevState) => {
-      return prevState + 1;
-    });
-  };
+	const handleBad = () => {
+		setBad((prevState) => {
+			return prevState + 1
+		})
+	}
 
-  return (
-    <>
-      <Header />
-      <Button
-        handleGood={handleGood}
-        handleNeutral={handleNeutral}
-        handleBad={handleBad}
-      />
-      <Statistics good={good} neutral={neutral} bad={bad} />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Button
+				handleGood={handleGood}
+				handleNeutral={handleNeutral}
+				handleBad={handleBad}
+			/>
+			<Statistics good={good} neutral={neutral} bad={bad} />
+		</>
+	)
 }
 
-export default App;
+export default App
--- part1/unicafe/src/StatisticLine.jsx
+++ part1/unicafe/src/StatisticLine.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 const StatisticLine = (props) => {
-  const { text, value } = props;
-  return (
-    <>
-      <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-      </tr>
-    </>
-  );
-};
+	const { text, value } = props
+	return (
+		<>
+			<tr>
+				<td>{text}</td>
+				<td>{value}</td>
+			</tr>
+		</>
+	)
+}
 
-export default StatisticLine;
+export default StatisticLine
